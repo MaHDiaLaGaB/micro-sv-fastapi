5f2dbe9cac6729904def4346ba1f7ed5495e07aa
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-03-28 00:07:05 +0300

    upload file function

--- app/main.py
+++ app/main.py
@@ -1,16 +1,44 @@
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import uuid
+import io
+from functools import lru_cache
+from fastapi import FastAPI, Depends, Request, File, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
 
+
+# ---- config ------
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_sittings():
+    return Settings()
+
+
+DEBUG = get_sittings().debug
+
+# ------ end config ------
+
+# ------ tell fast api to search for templates and uploaded files ------
 BASE_DIR = pathlib.Path(__file__).parent
-
+PHOTO_DIR = BASE_DIR / "media"
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
+# ------ end templates ---------
 
+
+# ------ routs views --------
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+# settings here to import the settings from the config settings
+async def root(request: Request, settings: Settings = Depends(get_sittings)):
     return templates.TemplateResponse("home.html", {'request': request, 'abc': 123})
 
 
@@ -22,3 +50,15 @@ def home_view():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+# upload file bt http post
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo(file: UploadFile = File(...), settings: Settings = Depends(get_sittings)):
+    file_bytes_str = io.BytesIO(await file.read())
+    file_name = pathlib.Path(file.filename)
+    file_type = file_name.suffix  # jpg or .txt
+    destination = PHOTO_DIR / f"{uuid.uuid1()}{file_type}"
+    with open(str(destination), 'wb') as out:
+        out.write(file_bytes_str.read())
+    return file
