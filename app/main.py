2e3b9ec87e18608525611c9dccbd95a5269a5687
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-03-28 22:40:51 +0300

    AssertionError: assert jpg in image/jpeg

--- app/main.py
+++ app/main.py
@@ -2,33 +2,37 @@ import pathlib
 import uuid
 import io
 from functools import lru_cache
-from fastapi import FastAPI, Depends, Request, File, UploadFile
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
+from fastapi import (
+    FastAPI, Depends,
+    Request, File,
+    UploadFile, HTTPException)
 
 
 # ---- config ------
 class Settings(BaseSettings):
     debug: bool = False
-    echo_active: bool = False
+    echo_active: bool = True
 
     class Config:
         env_file = ".env"
 
 
 @lru_cache()
-def get_sittings():
+def get_settings():
     return Settings()
 
 
-DEBUG = get_sittings().debug
+DEBUG = get_settings().debug
 
 # ------ end config ------
 
 # ------ tell fast api to search for templates and uploaded files ------
 BASE_DIR = pathlib.Path(__file__).parent
 PHOTO_DIR = BASE_DIR / "media"
+PHOTO_DIR.mkdir(exist_ok=True)
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
@@ -38,7 +42,7 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 # ------ routs views --------
 @app.get("/", response_class=HTMLResponse)
 # settings here to import the settings from the config settings
-async def root(request: Request, settings: Settings = Depends(get_sittings)):
+async def root(request: Request, settings: Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {'request': request, 'abc': 123})
 
 
@@ -54,11 +58,13 @@ async def say_hello(name: str):
 
 # upload file bt http post
 @app.post("/img-echo/", response_class=FileResponse)
-async def img_echo(file: UploadFile = File(...), settings: Settings = Depends(get_sittings)):
+async def img_echo(file: UploadFile = File(...), settings: Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="invalid endpoint", status_code=400)
     file_bytes_str = io.BytesIO(await file.read())
     file_name = pathlib.Path(file.filename)
     file_type = file_name.suffix  # jpg or .txt
     destination = PHOTO_DIR / f"{uuid.uuid1()}{file_type}"
     with open(str(destination), 'wb') as out:
         out.write(file_bytes_str.read())
-    return file
+    return destination
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app, BASE_DIR
+import shutil
+import time
 
 
 client = TestClient(app)
@@ -15,3 +17,15 @@ def test_post_root():
     response = client.post('/')
     assert response.status_code == 200
     assert 'application/json' in response.headers['content-type']
+
+
+def test_image_echo():
+    img_path = BASE_DIR / "images"
+    for path in img_path.glob("*"):
+        response = client.post('/img-echo/', files={"file": open(path, 'rb')})
+        assert response.status_code == 200
+        print(path.suffix)
+        print(response.headers['content-type'])
+        file_type = str(path.suffix).replace('.', '')
+        assert file_type in response.headers['content-type']
+
